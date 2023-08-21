a6c661591cc63f1bdd54a1889c943dbe493ff1d8
LN-Development <123782375+LN-Development@users.noreply.github.com>
2023-08-21 12:13:06 -0300

    Create functions.cs

---
+++ client/functions.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+public class LNCore
+{
+    public Dictionary<string, object> Functions { get; set; } = new Dictionary<string, object>();
+}
+
+
+
+public static class Exports
+{
+    public static LNCore GetCoreObject()
+    {
+        LNCore lnCore = new LNCore();
+        return lnCore;
+    }
+}
+
+//Pega informação do player
+public class LNCoreFunctions
+{
+    public static object GetPlayerData(Action<object> cb = null)
+    {
+        if (cb == null)
+            return LNCore.PlayerData;
+
+        cb(LNCore.PlayerData);
+        return null;
+    }
+}
+
+//Pega coordenada do player
+public class LNCoreFunctions
+{
+    public static Vector4 GetCoords(int entity)
+    {
+        Vector3 coords = API.GetEntityCoords(entity);
+        return new Vector4(coords.X, coords.Y, coords.Z, API.GetEntityHeading(entity));
+    }
+}
