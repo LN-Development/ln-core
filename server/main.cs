da1569143550798895efea351a5723f591e54ae4
Luan <123782375+LN-Development@users.noreply.github.com>
2023-08-21 11:49:12 -0300

    First step
    Basic source identifier

---
+++ server/main.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+public class LNCore
+{
+    public LNConfig Config { get; set; }
+    public LNShared Shared { get; set; }
+    public Dictionary<string, object> ClientCallbacks { get; set; } = new Dictionary<string, object>();
+    public Dictionary<string, object> ServerCallbacks { get; set; } = new Dictionary<string, object>();
+}
+
+public static class Exports
+{
+    public static LNCore GetCoreObject()
+    {
+        LNCore LNCore = new LNCore();
+        return LNCore;
+    }
+}
