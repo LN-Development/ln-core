b7892afd6e4ee7c0f81fb95ab769addc52f3c6ac
LN-Development <123782375+LN-Development@users.noreply.github.com>
2023-08-21 13:14:36 -0300

    Create notify.cs

---
+++ server/functions/notify.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+public class LNCore
+{
+    public LNConfig Config { get; set; }
+    public LNShared Shared { get; set; }
+    public Dictionary<string, object> ClientCallbacks { get; set; } = new Dictionary<string, object>();
+    public Dictionary<string, object> ServerCallbacks { get; set; } = new Dictionary<string, object>();
+}
+
+public static class Exports
+{
+    public static LNCore GetCoreObject()
+    {
+        LNCore lnCore = new LNCore();
+        return lnCore;
+    }
+}
+
+
+public class LNCoreFunctions
+{
+    public void Notify(int source, string text, string type, int length)
+    {
+        API.TriggerClientEvent("LNCore:Notify", source, text, type, length);
+    }
+}
