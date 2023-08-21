50c2d00eb77bd2757a8b69a4e798e08095996158
LN-Development <123782375+LN-Development@users.noreply.github.com>
2023-08-21 12:54:33 -0300

    Create itemshandling.cs

---
+++ server/itemshandling.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+public class LNCore
+{
+    public LNConfig Config { get; set; }
+    public LNShared Shared { get; set; }
+    public Dictionary<string, object> ClientCallbacks { get; set; } = new Dictionary<string, object>();
+    public Dictionary<string, object> ServerCallbacks { get; set; } = new Dictionary<string, object>();
+}
+
+public static class Exports
+{
+    public static LNCore GetCoreObject()
+    {
+        LNCore lnCore = new LNCore();
+        return lnCore;
+    }
+}
+
+
+public class LNCoreFunctions
+{
+    public void CreateUseableItem(string item, dynamic data)
+    {
+        LNCore.UsableItems[item] = data;
+    }
+
+
+    public dynamic CanUseItem(string item)
+    {
+        if (LNCore.UsableItems.ContainsKey(item))
+        {
+            return LNCore.UsableItems[item];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void UseItem(int source, string item)
+    {
+        if (API.GetResourceState("ln-inventory") == "missing") return;
+        API.Export("ln-inventory", "UseItem", source, item);
+    }
+}
