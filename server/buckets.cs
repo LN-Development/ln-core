2d8b46a50123b74b5fb02969a2496cd8ee039617
LN-Development <123782375+LN-Development@users.noreply.github.com>
2023-08-21 12:31:43 -0300

    Create buckets.cs

---
+++ server/buckets.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+
+public class LNCoreFunctions
+{
+    public List<object> GetQBPlayers()
+    {
+        return LNCore.Players;
+    }
+
+    public (Dictionary<string, object>, Dictionary<string, object>) GetBucketObjects()
+    {
+        return (LNCore.PlayerBuckets, LNCore.EntityBuckets);
+    }
+
+    public bool SetPlayerBucket(int source, string bucket)
+    {
+        if (source != 0 && !string.IsNullOrEmpty(bucket))
+        {
+            string plicense = LNCoreFunctions.GetIdentifier(source, "license");
+            SetPlayerRoutingBucket(source, bucket);
+            LNCore.PlayerBuckets[plicense] = new { id = source, bucket = bucket };
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static string GetIdentifier(int source, string idtype)
+    {
+        var identifiers = API.GetPlayerIdentifiers(source).ToList();
+        foreach (var identifier in identifiers)
+        {
+            if (identifier.Contains(idtype))
+            {
+                return identifier;
+            }
+        }
+        return null;
+    }
+
+    public bool SetEntityBucket(int entity, string bucket)
+    {
+        if (entity != 0 && !string.IsNullOrEmpty(bucket))
+        {
+            SetEntityRoutingBucket(entity, bucket);
+            LNCore.EntityBuckets[entity] = new { id = entity, bucket = bucket };
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public List<int> GetPlayersInBucket(string bucket)
+    {
+        List<int> currBucketPool = new List<int>();
+        if (LNCore.PlayerBuckets != null && LNCore.PlayerBuckets.Any())
+        {
+            foreach (var v in LNCore.PlayerBuckets)
+            {
+                if (v.Value.bucket == bucket)
+                {
+                    currBucketPool.Add(v.Value.id);
+                }
+            }
+            return currBucketPool;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public List<int> GetEntitiesInBucket(string bucket)
+    {
+        List<int> currBucketPool = new List<int>();
+        if (LNCore.EntityBuckets != null && LNCore.EntityBuckets.Any())
+        {
+            foreach (var v in LNCore.EntityBuckets)
+            {
+                if (v.Value.bucket == bucket)
+                {
+                    currBucketPool.Add(v.Value.id);
+                }
+            }
+            return currBucketPool;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
